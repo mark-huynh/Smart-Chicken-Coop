2814fbfbe8288cb500c0b182df68f42c2e6bcac3
mcbeenick <55850596+mcbeenick@users.noreply.github.com>
2021-02-18 14:21:39 -0800

    Fixed variable names and started power consumption

--- dimmer_test.ino
+++ dimmer_test.ino
@@ -7,13 +7,16 @@
  */
  
 #define LAMP_MALFUNCTION_TRESH 1000 //Comparison value for ADC brightness measurement
+#define LAMP_WATTAGE 4              //Wattage rating of lamp bulb
 
 const int dimmerPin = 3;       //Pin for PWM control of dimmer board
 const int lampPhotoPin = A6;  //Photoresistor used for heat lamp
 byte brightness_setting = 255; //Brightness setting for lamp
 bool lamp_malfunction = 0;    //Indicates malfunction when high 
 int currTime[3];              //Time array, holds dummy values for simulation
-
+int lampStartTime[3];         //On-time for the heat lamp for the current day in hours
+float lampTime;
+float power_consumption = 0;    //Power consumed by lamp in Watt-hours
 
 void setup() {
   Serial.begin(57600);
@@ -62,6 +65,9 @@ Serial.println("");
  * on current time and how bright it should be
  */
 void lamp_controller(){
+  update_lamp_time();
+  int old_brightness = brightness_setting; //Saves current brightness
+    
   if(brightness_setting == 255) //Checks lamp when fully turned on
     lamp_malfunction = check_lamp();
   if((currTime[0] >= 6) && (currTime[0] < 9)) { //Turns on lamp in early morning
@@ -77,7 +83,30 @@ void lamp_controller(){
     brightness_setting = 0;   //Turn off lamp at all other times
   }
   analogWrite(dimmerPin, brightness_setting); //Set signal for brightness
+
+  //Records the time when lamp goes from off to on 
+  if((old_brightness == 0) && (brightness_setting != 0)) {
+    lampStartTime = currTime[0];
+    lampStartTime += currTime[1]/60;
+    lampStartTime += currTime[2]/3600;
+  }
     
+}
+
+void update_lamp_time() {
+  float timeHours = 0;   //The current time of day expressed in hours
+  timeHours += currTime[0];
+  timeHours += currTime[1]/60;
+  timeHours += currTime[2]/3600;
+  if(currTime[0] == 0) { //Reset time tally at midnight
+    lampTime = 0;
+  }
+  lampTime = timeHours - lampStartTime; 
+  
+}
+
+float get_power_consumption() {
+  //lampTime = currTime;
 }
 
 /* Func: dim_lamp
@@ -98,7 +127,7 @@ void dim_lamp() {
  * to remain accurate. 
  */
 bool check_lamp() {
-  int lamp_brightness = analogRead(lampThermistorPin);  
+  int lamp_brightness = analogRead(lampPhotoPin);  
   if(lamp_brightness < LAMP_MALFUNCTION_TRESH) //Check if ADC reading is at corresponding light threshold
     return 1;
   return 0;
